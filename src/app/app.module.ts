2d49fc6733ab7b7a2f25f564dfae32c411806fe8
Anna <anna-theresia.Dersch@iem.thm.de>
2022-12-20 07:45:14 +0100

    router works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { OnepagerComponent } from './onepager/onepager.component';
@@ -9,6 +10,11 @@ import { VorstellungComponent } from './vorstellung/vorstellung.component';
 import { SpielregelnComponent } from './spielregeln/spielregeln.component';
 import { ErklaerungComponent } from './erklaerung/erklaerung.component';
 
+const appRoutes: Routes=[
+  {path: 'start', component: OnepagerComponent},
+  {path: 'escape', component: EscaperoomComponent},
+  {path: '', redirectTo: '/start', pathMatch:'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +26,11 @@ import { ErklaerungComponent } from './erklaerung/erklaerung.component';
     ErklaerungComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
