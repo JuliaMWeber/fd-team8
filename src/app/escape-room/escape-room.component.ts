33802fb26c4bc7e73a13340a627c8ebf08e838a8
Anna <anna-theresia.dersch@iem.thm.de>
2022-12-21 10:28:20 +0100

    something in the escape to see

--- src/app/escape-room/escape-room.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-escape-room',
-  templateUrl: './escape-room.component.html',
-})
-export class EscapeRoomComponent {
-
-}
