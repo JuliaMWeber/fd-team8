24814426bab644b473bb00e547d75bda6c7613ef
jmwb03 <julia.melanie.weber@iem.thm.de>
2022-12-30 11:25:21 +0100

    test some angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+
+//neu
+import {
+  trigger, state, style, animate, transition, query, group
+ } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  animations: [
+    //animation triggers go here
+    
+  ]
 })
 export class AppComponent {
   title = 'untitled4';
