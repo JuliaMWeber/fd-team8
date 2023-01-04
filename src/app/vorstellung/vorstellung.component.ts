84e1aed598b3da5cf8f24a28bbbe7c2e4cb34f6f
jmwb03 <julia.melanie.weber@iem.thm.de>
2023-01-04 14:32:15 +0100

    bug fixed

--- src/app/vorstellung/vorstellung.component.ts
+++ src/app/vorstellung/vorstellung.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-vorstellung',
   templateUrl: './vorstellung.component.html',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2500ms', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translate(0, 0)'}),
+        animate('2500ms', style({ opacity: 0, transform: 'translate(-200px, 0'}))
+      ])
+    ])
+  ]
 })
 export class VorstellungComponent {
 
