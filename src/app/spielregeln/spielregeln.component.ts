780c96049ec7e6e7acdd4967b456a140c54c08ca
jmwb03 <julia.melanie.weber@iem.thm.de>
2023-01-20 22:53:37 +0100

    animation

--- src/app/spielregeln/spielregeln.component.ts
+++ src/app/spielregeln/spielregeln.component.ts
@@ -20,20 +20,7 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
     ])
   ]
   
-/*
-  animations: [
-    trigger('fade', [ 
-      transition(':enter', [
-        style({opacity:0}),
-        animate(1000, style({opacity:1}))
-      ]),
-      transition(':leave',[
-        style({opacity:1}),
-        animate(1000, style({opacity:0}))
-      ] )
-    ])
-  ]
-  */
+
 })
 
 export class SpielregelnComponent {
