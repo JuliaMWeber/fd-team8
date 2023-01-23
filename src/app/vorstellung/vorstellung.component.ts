2b958e23f317e2c4511ea85f3208ddbe177e8d65
jmwb03 <julia.melanie.weber@iem.thm.de>
2023-01-23 12:25:14 +0100

    test

--- src/app/vorstellung/vorstellung.component.ts
+++ src/app/vorstellung/vorstellung.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
+import { trigger, state, style, animate, transition, query, group } from '@angular/animations';
 
 @Component({
   selector: 'app-vorstellung',
   templateUrl: './vorstellung.component.html',
   animations: [
     trigger('fadeIn', [
+    
       transition(':enter', [
         style({ opacity: 0 }),
         animate('2500ms', style({ opacity: 1 }))
@@ -14,7 +15,21 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
         style({ opacity: 1, transform: 'translate(0, 0)'}),
         animate('2500ms', style({ opacity: 0, transform: 'translate(-200px, 0'}))
       ])
-    ])
+    ]),
+    /*
+    trigger('fadeInImg', [
+    
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.2s 1500ms ease-in-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translate(0, 0)'}),
+        animate('0.2s 1500ms ease-in-out' , style({ opacity: 0, transform: 'translate(-200px, 0'}))
+      ])
+    ]),
+    */
+
   ]
 })
 
