1a26cef6ef5373da193503ff7ab6542790ab238e
Anna <anna-theresia.dersch@iem.thm.de>
2023-01-24 09:02:38 +0100

    javascript for the questions work

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,16 +1,44 @@
-var counterValue = document.getElementsByClassName('counterValue').innerHTML;
-window.addEventListener('click', colorChange())
+var counterVal = 0;
+
+function incrementClick() {
+  if (counterVal<100){
+  updateDisplay(counterVal += 10);
+  }
+
+}
+
+function updateDisplay(val) {
+  document.getElementById('counterValueMitte', 'counterLinks', 'counterRechts').innerHTML = val;
+
+  if (val < 25) {
+    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ff0000";
+  } else if (val < 50) {
+    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ffa500";
+  } else if (val > 75 && val < 99) {
+    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ddff00";
+  } else if (val >= 100) {
+    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#00ff00";
+  }
+  if (val === 100) {
+    alert("Herzlichen Glückwunsch. Du wirst die Klausur bestehen!")
+  }
+
+}
 
 
-function colorChange(){
-    var zahl = parseInt(counterValue);
-    if (zahl <= 25) {
-      return document.getElementsByClassName(counter).backgroundColor = "#ff0000";
-    } else if (counterValue < 50) {
-      return document.getElementsByClassName(counter).backgroundColor = "#ffa500";
-    } else if (zahl <= 75) {
-      return document.getElementsByClassName(counter).backgroundColor = "#ddff00";
-    } else {
-      return document.getElementsByClassName(counter).backgroundColor = "#00ff00";
+function ValidateForm(fragenName, richtigeAntwort, buttonName) {
+  let moeglichkeiten = document.getElementsByName(fragenName);
+  let antwort = document.getElementById(richtigeAntwort).value;
+  let button = document.getElementById(buttonName);
+
+  for (let j = 0; j < moeglichkeiten.length; j++) {
+    if (moeglichkeiten[j].checked === true) {
+      const userAntwort = moeglichkeiten[j].value;
+      if (userAntwort === antwort) {
+        incrementClick();
+        button.disabled=true;
+
+      }
     }
+  }
 }
