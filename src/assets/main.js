aa1335a5e4cd45eb3b0a79320b7cbac692328792
Anna <anna-theresia.dersch@iem.thm.de>
2023-01-25 13:25:41 +0100

    questions in every room works

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,23 +1,102 @@
-var counterVal = 0;
+let counterVal = localStorage.getItem(counterValUebergabe());
 
-function incrementClick() {
-  if (counterVal<100){
-  updateDisplay(counterVal += 10);
+
+function incrementClickLinks() {
+  if (counterVal < 100) {
+    updateDisplayLinks(counterVal += 6);
+  }
+}
+
+function incrementClickRechts() {
+  if (counterVal < 100) {
+    updateDisplayRechts(counterVal += 6);
+  }
+}
+
+function incrementClickMitte() {
+  if (counterVal < 100) {
+    updateDisplayMitte(counterVal += 6
+    );
+  }
+}
+
+function counterValUebergabe() {
+  let linkerVal = localStorage.getItem('counterValueLinks');
+  let mittlererVal = localStorage.getItem('counterValueMitte');
+  let rechterVal = localStorage.getItem('counterValueRechts');
+
+  if (linkerVal < rechterVal) {
+    linkerVal = rechterVal;
+    return linkerVal
+  } else if (linkerVal < mittlererVal) {
+    linkerVal = mittlererVal;
+    return linkerVal
+  }
+  if (rechterVal < linkerVal) {
+    rechterVal = linkerVal;
+    return rechterVal
+  } else if (rechterVal < mittlererVal) {
+    rechterVal = mittlererVal;
+    return rechterVal
+  }
+  if (mittlererVal < rechterVal) {
+    mittlererVal = rechterVal
+    return mittlererVal
+  } else if (mittlererVal < linkerVal) {
+    mittlererVal = linkerVal
+    return mittlererVal
+  }
+}
+
+function updateDisplayMitte(val) {
+  document.getElementById('counterValueMitte').innerHTML = val;
+
+
+  if (val < 25) {
+    document.getElementById('counterMitte').style.backgroundColor = "#ff0000";
+  } else if (val < 50) {
+    document.getElementById('counterMitte').style.backgroundColor = "#ffa500";
+  } else if (val > 75 && val < 99) {
+    document.getElementById('counterMitte').style.backgroundColor = "#ddff00";
+  } else if (val >= 100) {
+    document.getElementById('counterMitte').style.backgroundColor = "#00ff00";
+  }
+  if (val === 100) {
+    alert("Herzlichen Glückwunsch. Du wirst die Klausur bestehen!")
   }
 
 }
 
-function updateDisplay(val) {
-  document.getElementById('counterValueMitte', 'counterLinks', 'counterRechts').innerHTML = val;
+function updateDisplayLinks(val) {
+  document.getElementById('counterValueLinks').innerHTML = val;
+
 
   if (val < 25) {
-    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ff0000";
+    document.getElementById('counterLinks').style.backgroundColor = "#ff0000";
   } else if (val < 50) {
-    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ffa500";
+    document.getElementById('counterLinks').style.backgroundColor = "#ffa500";
   } else if (val > 75 && val < 99) {
-    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#ddff00";
+    document.getElementById('counterLinks').style.backgroundColor = "#ddff00";
   } else if (val >= 100) {
-    document.getElementById('counterMitte', 'counterRechts', 'counterLinks').style.backgroundColor = "#00ff00";
+    document.getElementById('counterLinks').style.backgroundColor = "#00ff00";
+  }
+  if (val === 100) {
+    alert("Herzlichen Glückwunsch. Du wirst die Klausur bestehen!")
+  }
+
+}
+
+function updateDisplayRechts(val) {
+  document.getElementById('counterValueRechts').innerHTML = val;
+
+  if (val < 25) {
+    document.getElementById('counterRechts').style.backgroundColor = "#ff0000";
+  } else if (val < 50) {
+    document.getElementById('counterRechts').style.backgroundColor = "#ffa500";
+  } else if (val > 75 && val < 99) {
+    document.getElementById('counterRechts').style.backgroundColor = "#ddff00";
+  } else if (val >= 100) {
+    document.getElementById('counterRechts').style.backgroundColor = "#00ff00";
   }
   if (val === 100) {
     alert("Herzlichen Glückwunsch. Du wirst die Klausur bestehen!")
@@ -26,7 +105,7 @@ function updateDisplay(val) {
 }
 
 
-function ValidateForm(fragenName, richtigeAntwort, buttonName) {
+function ValidateFormMitte(fragenName, richtigeAntwort, buttonName) {
   let moeglichkeiten = document.getElementsByName(fragenName);
   let antwort = document.getElementById(richtigeAntwort).value;
   let button = document.getElementById(buttonName);
@@ -35,10 +114,46 @@ function ValidateForm(fragenName, richtigeAntwort, buttonName) {
     if (moeglichkeiten[j].checked === true) {
       const userAntwort = moeglichkeiten[j].value;
       if (userAntwort === antwort) {
-        incrementClick();
-        button.disabled=true;
+        incrementClickMitte();
+        button.disabled = true;
+      }
+    }
+  }
+}
+
+function ValidateFormLinks(fragenName, richtigeAntwort, buttonName) {
+  let moeglichkeiten = document.getElementsByName(fragenName);
+  let antwort = document.getElementById(richtigeAntwort).value;
+  let button = document.getElementById(buttonName);
+
+  for (let j = 0; j < moeglichkeiten.length; j++) {
+    if (moeglichkeiten[j].checked === true) {
+      const userAntwort = moeglichkeiten[j].value;
+      if (userAntwort === antwort) {
+        incrementClickLinks();
+        button.disabled = true;
 
       }
     }
   }
 }
+
+function ValidateFormRechts(fragenName, richtigeAntwort, buttonName) {
+  let moeglichkeiten = document.getElementsByName(fragenName);
+  let antwort = document.getElementById(richtigeAntwort).value;
+  let button = document.getElementById(buttonName);
+
+  for (let j = 0; j < moeglichkeiten.length; j++) {
+    if (moeglichkeiten[j].checked === true) {
+      const userAntwort = moeglichkeiten[j].value;
+      if (userAntwort === antwort) {
+        incrementClickRechts();
+        button.disabled = true;
+
+      }
+    }
+  }
+}
+
+
+
