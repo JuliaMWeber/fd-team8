de5b7dd13b291523406c33aa3cd5157a1ee1b25d
jmwb03 <julia.melanie.weber@iem.thm.de>
2023-02-05 17:27:06 +0100

    add forgotten burger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  
 
+  
+constructor() { }
+
+  menu_icon_variable: boolean = false;
+  menuVariable: boolean = false;
+  ngOnInit(): void {
+  }
+  openMenu() {
+    this.menuVariable =! this.menuVariable;
+    this.menu_icon_variable =! this.menu_icon_variable;
+  }
 }
